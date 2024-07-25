708f6311a243f7626908f096ded7763e58a6b52e
Md-IrfanS <mohammedirfan3.it@gmail.com>
2024-07-25 22:34:49 +0530

    route structure

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const app = express();
 const PORT = 5362;
 
 app.get("/", (req, res) => {
-  res.json({message: "Hello server"})
+  res.json({ message: "Hello server" });
 });
 
+// CRUD functionality of eCommerce
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+// R - For Reading products
+app.get("/products", () => {});
+
+// C - For Creating products 
+app.post("/products", () => {});
+
+// U - For Updating products
+app.put("/products/:id", () => {});
+
+// D - For Deleting product
+app.delete("/products/:id", () => {});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
